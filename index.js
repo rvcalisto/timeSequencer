6adc5ac2df10f8b058b7d1dee48535ea2e017da6
rvcalisto <16952101+rvcalisto@users.noreply.github.com>
2023-06-20 01:43:03 -0300

    create a request-notification button when permission isn't detected for such

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+/**
+ * Creates a notification request button when permission isn't already detected.
+ */
+function createNotificationButton() {
+  if (Notification.permission === 'granted') return
+
+  const allowNotifyBtn = document.createElement('button')
+  allowNotifyBtn.textContent = '🔔❔'
+  allowNotifyBtn.title = 'Allow notifications for timeout alerts while this tab is unfocused?'
+
+  allowNotifyBtn.onclick = async () => {
+    const permission = await Notification.requestPermission()
+    if (permission === 'granted') allowNotifyBtn.remove()
+  }
+
+  const estimatedTimeLabel = document.getElementById('executeFrame')
+  estimatedTimeLabel.before(allowNotifyBtn)
+}
+
+
 onload = function startApp() {
   Timer.newItem()
+  createNotificationButton() 
 }
 
